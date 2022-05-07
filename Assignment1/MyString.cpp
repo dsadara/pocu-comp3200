39ef7726b71d8926a91619c643bb2c677b6daf38
dsadara <lim971230@gmail.com>
2022-05-07 12:50:35 +0900

    메모리 침범 문제 해결
    동적 할당 메모리 크기와, memcpy시 크기 조정

--- Assignment1/MyString.cpp
+++ Assignment1/MyString.cpp
@@ -3,104 +3,112 @@
 
 namespace assignment1
 {
-    MyString::MyString(const char* s)
-        : mSize(strlen(s))
-    {
-        mString = new char[mSize + 1];  // 널 문자를 위해 + 1을 하였음 -> 이게 맞나 알아보기
-        memcpy(mString, s, mSize);
-    }
+	MyString::MyString(const char* s)
+		: mSize(strlen(s) + 1)
+	{
+		mString = new char[mSize];
+		memset(mString, 0, mSize);
+		memcpy(mString, s, mSize);
+	}
 
-    MyString::MyString(const MyString& other)
-        : mSize(other.mSize)
-    {
-        mString = new char[mSize + 1];
-        memcpy(mString, other.mString, mSize);
-    }
+	MyString::MyString(const MyString& other)
+		: mSize(other.mSize)
+	{
+		mString = new char[mSize];
+		memset(mString, 0, mSize);
+		memcpy(mString, other.mString, mSize);
+	}
 
-    MyString::~MyString()
-    {   
-        delete[] mString;   // 생성할 때 배열로 생성했으므로 배열표시[]를 넣어줌dd
-    }
+	MyString::~MyString()
+	{
+		delete[] mString;   // 생성할 때 배열로 생성했으므로 배열표시[]를 넣어줌
+	}
 
-    unsigned int MyString::GetLength() const
-    {
-        return MyString::mSize;
-    }
+	unsigned int MyString::GetLength() const
+	{
+		return mSize;
+	}
 
-    const char* MyString::GetCString() const
-    {
-        return MyString::mString;
-    }
+	const char* MyString::GetCString() const
+	{
+		return mString;
+	}
 
-    void MyString::Append(const char* s)
-    {
-        int sSize = strlen(s);
-        char* result = new char[mSize + sSize + 1];
-        memcpy(result, mString, mSize);
-        delete[] mString;   // 이전 mString 메모리 해제 
-        strncat(result, s, sSize);
-        mString = result;
-    }
+	void MyString::Append(const char* s)
+	{
+		int sSize = strlen(s);
+		char* result = new char[mSize + sSize + 1];
+		memcpy(result, mString, mSize);
+		delete[] mString;   // 이전 mString 메모리 해제kk
+		strncat(result, s, sSize);
+		mSize = mSize + sSize;
+		mString = result;
+	}
 
-    MyString MyString::operator+(const MyString& other) const
-    {
-        return MyString("temporary");
-    }
+	MyString MyString::operator+(const MyString& other) const
+	{
+		int size = mSize + other.GetLength();
+		char* result = new char[size];
+		memset(result, 0, size);
+		memcpy(result, mString, mSize);
+		strncat(result, other.GetCString(), other.GetLength());
+		return MyString(result);
+	}
 
-    int MyString::IndexOf(const char* s)
-    {
-        return 0;
-    }
+	int MyString::IndexOf(const char* s)
+	{
+		return 0;
+	}
 
-    int MyString::LastIndexOf(const char* s)
-    {
-        return 0;
-    }
+	int MyString::LastIndexOf(const char* s)
+	{
+		return 0;
+	}
 
-    void MyString::Interleave(const char* s)
-    {
-    }
+	void MyString::Interleave(const char* s)
+	{
+	}
 
-    bool MyString::RemoveAt(unsigned int i)
-    {
-        return false;
-    }
+	bool MyString::RemoveAt(unsigned int i)
+	{
+		return false;
+	}
 
-    void MyString::PadLeft(unsigned int totalLength)
-    {
-    }
+	void MyString::PadLeft(unsigned int totalLength)
+	{
+	}
 
-    void MyString::PadLeft(unsigned int totalLength, const char c)
-    {
-    }
+	void MyString::PadLeft(unsigned int totalLength, const char c)
+	{
+	}
 
-    void MyString::PadRight(unsigned int totalLength)
-    {
-    }
+	void MyString::PadRight(unsigned int totalLength)
+	{
+	}
 
-    void MyString::PadRight(unsigned int totalLength, const char c)
-    {
-    }
+	void MyString::PadRight(unsigned int totalLength, const char c)
+	{
+	}
 
-    void MyString::Reverse()
-    {
-    }
+	void MyString::Reverse()
+	{
+	}
 
-    bool MyString::operator==(const MyString& rhs) const
-    {
-        return false;
-    }
+	bool MyString::operator==(const MyString& rhs) const
+	{
+		return false;
+	}
 
-    MyString& MyString::operator=(const MyString& rhs)
-    {
-        return *this;
-    }
+	MyString& MyString::operator=(const MyString& rhs)
+	{
+		return *this;
+	}
 
-    void MyString::ToLower()
-    {
-    }
+	void MyString::ToLower()
+	{
+	}
 
-    void MyString::ToUpper()
-    {
-    }
+	void MyString::ToUpper()
+	{
+	}
 }
