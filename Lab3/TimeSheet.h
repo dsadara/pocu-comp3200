bb731185aa36dd07f2d08ee84945086ed3a641a8
DESKTOP-2C9E12I\lim97 <lim971230@gmail.com>
2022-05-16 17:21:10 +0900

    포맷팅 수정

--- Lab3/TimeSheet.h
+++ Lab3/TimeSheet.h
@@ -4,25 +4,25 @@
 
 namespace lab3
 {
-    class TimeSheet
-    {
-    public:
-        TimeSheet(const char* name, unsigned int maxEntries);
-        TimeSheet(TimeSheet& other);
-        void operator=(const TimeSheet& rhs);
-        ~TimeSheet();
-        void AddTime(int timeInHours);
-        int GetTimeEntry(unsigned int index) const;
-        int GetTotalTime() const;
-        float GetAverageTime() const;
-        float GetStandardDeviation() const;
-        const std::string& GetName() const;
+	class TimeSheet
+	{
+	public:
+		TimeSheet(const char* name, unsigned int maxEntries);
+		TimeSheet(TimeSheet& other);
+		void operator=(const TimeSheet& rhs);
+		~TimeSheet();
+		void AddTime(int timeInHours);
+		int GetTimeEntry(unsigned int index) const;
+		int GetTotalTime() const;
+		float GetAverageTime() const;
+		float GetStandardDeviation() const;
+		const std::string& GetName() const;
 
-    private:
-        unsigned int mMaxEntries;
-        int* mWorkTimes;
-        unsigned int mWorkTimesIndex;
-        std::string mName;
-        // 필요에 따라 private 변수와 메서드를 추가하세요.
-    };
+	private:
+		unsigned int mMaxEntries;
+		int* mWorkTimes;
+		unsigned int mWorkTimesIndex;
+		std::string mName;
+		// 필요에 따라 private 변수와 메서드를 추가하세요.
+	};
 }
