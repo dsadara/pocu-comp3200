e628127f11c7dde6aa3141a7659ad99891a21424
dsadara <lim971230@gmail.com>
2022-07-05 12:37:53 +0900

    멤버로 있는 스마트 스택 제거
    NoMemIssue 해결을 위함

--- Assignment3/SmartQueue.h
+++ Assignment3/SmartQueue.h
@@ -26,18 +26,22 @@ namespace assignment3
 		unsigned int GetCount();
 	private:
 		std::queue<T> mQueue;
-		SmartStack<T> mSmartStack;
+		double mSum;
+		double mSquaredSum;
 	};
 
 	template <typename T>
 	SmartQueue<T>::SmartQueue()
+		: mSum(0.0)
+		, mSquaredSum(0.0)
 	{
 	}
 
 	template <typename T>
 	SmartQueue<T>::SmartQueue(const SmartQueue<T>& other)
 		: mQueue(other.mQueue)
-		, mSmartStack(other.mSmartStack)
+		, mSum(other.mSum)
+		, mSquaredSum(other.mSquaredSum)
 	{
 	}
 
@@ -49,8 +53,9 @@ namespace assignment3
 			return *this;
 		}
 
+		mSum = rhs.mSum;
+		mSquaredSum = rhs.mSquaredSum;
 		mQueue = rhs.mQueue;
-		mSmartStack = rhs.mSmartStack;
 
 		return *this;
 	}
@@ -65,7 +70,8 @@ namespace assignment3
 	void SmartQueue<T>::Enqueue(T number)
 	{
 		mQueue.push(number);
-		mSmartStack.Push(number);
+		mSum += static_cast<double>(number);
+		mSquaredSum += static_cast<double>(number) * static_cast<double>(number);
 	}
 
 	template <typename T>
@@ -79,62 +85,96 @@ namespace assignment3
 	{
 		T returnValue = mQueue.front();
 		mQueue.pop();
-
-		// 스택은 맨 앞에 원소를 pop
-		int smartStackSize = mSmartStack.GetCount();
-		T* stackArray = new T[smartStackSize];
-		for (int i = 0; i < smartStackSize; i++)
-		{
-			stackArray[i] = mSmartStack.Pop();
-		}
-		for (int i = smartStackSize - 2; i >= 0; i--)
-		{
-			mSmartStack.Push(stackArray[i]);
-		}
-		delete[] stackArray;
+		mSum -= static_cast<double>(returnValue);
+		mSquaredSum -= static_cast<double>(returnValue) * static_cast<double>(returnValue);
 		return returnValue;
 	}
 
 	template <typename T>
 	T SmartQueue<T>::GetMax()
 	{
-		return mSmartStack.GetMax();
+		int queueSize = mQueue.size();
+		T* qArray = new T[queueSize];
+		T max = std::numeric_limits<T>::lowest();
+		for (int i = 0; i < queueSize; i++)
+		{
+			T element = mQueue.front();
+			if (max < element)
+			{
+				max = element;
+			}
+			qArray[i] = element;
+			mQueue.pop();
+
+		}
+		// 큐 원소 다시 주워담기
+		for (int i = 0; i < queueSize; i++)
+		{
+			mQueue.push(qArray[i]);
+		}
+
+		delete[] qArray;
+		return max;
 	}
 
 	template <typename T>
 	T SmartQueue<T>::GetMin()
 	{
-		return mSmartStack.GetMin();
+		int queueSize = mQueue.size();
+		T* qArray = new T[queueSize];
+		T min = std::numeric_limits<T>::max();
+		for (int i = 0; i < queueSize; i++)
+		{
+			T element = mQueue.front();
+			if (min > element)
+			{
+				min = element;
+			}
+			qArray[i] = element;
+			mQueue.pop();
+
+		}
+		// 큐 원소 다시 주워담기
+		for (int i = 0; i < queueSize; i++)
+		{
+			mQueue.push(qArray[i]);
+		}
+
+		delete[] qArray;
+		return min;
 	}
 
 	template <typename T>
 	double SmartQueue<T>::GetAverage()
 	{
-		return mSmartStack.GetAverage();
+		return mSum / static_cast<double>(mQueue.size());
 	}
 
 	template <typename T>
 	T SmartQueue<T>::GetSum()
 	{
-		return mSmartStack.GetSum();
+		return static_cast<T>(mSum);
 	}
 
 	template <typename T>
 	double SmartQueue<T>::GetVariance()
 	{
-		return mSmartStack.GetVariance();
+		double avgSquare = GetAverage();
+		avgSquare *= avgSquare;
+		double squareAvg = mSquaredSum / static_cast<double>(mQueue.size());
+		return squareAvg - avgSquare;
 	}
 
 	template <typename T>
 	double SmartQueue<T>::GetStandardDeviation()
 	{
-		return mSmartStack.GetStandardDeviation();
+		return std::sqrt(GetVariance());
 	}
 
 	template <typename T>
 	unsigned int SmartQueue<T>::GetCount()
 	{
-		return mSmartStack.GetCount();
+		return mQueue.size();
 	}
 }
 
