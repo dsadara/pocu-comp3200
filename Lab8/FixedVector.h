4206d2854f40e9f59eb7155840403f6c96ed77c7
dsadara <lim971230@gmail.com>
2022-07-07 16:06:45 +0900

    수정이 필요 없는 매개변수이므로 const 추가

--- Lab8/FixedVector.h
+++ Lab8/FixedVector.h
@@ -7,11 +7,11 @@ namespace lab8
 	{
 	public:
 		FixedVector();
-		bool Add(T& t);
-		bool Remove(T& t);
+		bool Add(const T& t);
+		bool Remove(const T& t);
 		const T& Get(unsigned int index);
 		T& operator[](unsigned int index);
-		int GetIndex(T& t);
+		int GetIndex(const T& t);
 		size_t GetSize();
 		size_t GetCapacity();
 	private:
@@ -26,7 +26,7 @@ namespace lab8
 	}
 
 	template<typename T, size_t N>
-	bool FixedVector<T, N>::Add(T& t)
+	bool FixedVector<T, N>::Add(const T& t)
 	{
 		if (mSize > N - 1)
 		{
@@ -37,7 +37,7 @@ namespace lab8
 	}
 
 	template<typename T, size_t N>
-	bool FixedVector<T, N>::Remove(T& t)
+	bool FixedVector<T, N>::Remove(const T& t)
 	{
 		int i = GetIndex(t);
 		if (i == -1)
@@ -65,7 +65,7 @@ namespace lab8
 	}
 
 	template<typename T, size_t N>
-	int FixedVector<T, N>::GetIndex(T& t)
+	int FixedVector<T, N>::GetIndex(const T& t)
 	{
 		for (size_t i = 0; i < mSize; i++)
 		{
